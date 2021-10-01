5c7c0fc6446a5c740a59e942e35446a89b0f981a
Riccardo Damiani <37109009+damianiRiccardo90@users.noreply.github.com>
2021-10-01 17:47:02 +0100

    Update keras_darknet19.py
    Changed incorrect import

--- C4-Convolutional_Neural_Networks/W3-Object_detection/yad2k/models/keras_darknet19.py
+++ C4-Convolutional_Neural_Networks/W3-Object_detection/yad2k/models/keras_darknet19.py
@@ -4,7 +4,7 @@ from functools import partial
 
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.normalization import BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
 from keras.models import Model
 from keras.regularizers import l2
 
