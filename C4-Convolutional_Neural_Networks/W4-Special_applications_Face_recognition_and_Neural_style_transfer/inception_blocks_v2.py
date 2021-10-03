a4a0c1c5a6bb2f1afb1020fc300c3eefa761c982
Riccardo Damiani <37109009+damianiRiccardo90@users.noreply.github.com>
2021-10-03 08:28:30 +0100

    Update inception_blocks_v2.py
    Fixed wrong import

--- C4-Convolutional_Neural_Networks/W4-Special_applications_Face_recognition_and_Neural_style_transfer/inception_blocks_v2.py
+++ C4-Convolutional_Neural_Networks/W4-Special_applications_Face_recognition_and_Neural_style_transfer/inception_blocks_v2.py
@@ -5,7 +5,7 @@ from numpy import genfromtxt
 from keras import backend as K
 from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
 from keras.models import Model
-from keras.layers.normalization import BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
 from keras.layers.pooling import MaxPooling2D, AveragePooling2D
 import fr_utils
 from keras.layers.core import Lambda, Flatten, Dense
