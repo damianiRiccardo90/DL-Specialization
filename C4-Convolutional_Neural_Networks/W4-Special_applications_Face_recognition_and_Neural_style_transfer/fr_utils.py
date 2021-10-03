6703763e782d5ca12fd9308e5dec1d040a8f11ae
Riccardo Damiani <37109009+damianiRiccardo90@users.noreply.github.com>
2021-10-03 08:26:08 +0100

    Update fr_utils.py
    Wrong import fixed

--- C4-Convolutional_Neural_Networks/W4-Special_applications_Face_recognition_and_Neural_style_transfer/fr_utils.py
+++ C4-Convolutional_Neural_Networks/W4-Special_applications_Face_recognition_and_Neural_style_transfer/fr_utils.py
@@ -7,7 +7,7 @@ import cv2
 from numpy import genfromtxt
 from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
 from keras.models import Model
-from keras.layers.normalization import BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
 from keras.layers.pooling import MaxPooling2D, AveragePooling2D
 import h5py
 import matplotlib.pyplot as plt
